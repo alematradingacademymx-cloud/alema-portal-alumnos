931a3d1030df5d6d59327cdd0feaba2095888b67
alematradingacademymx-cloud <alematradingacademymx@gmail.com>
2026-09-04 02:03:04 -0600

    Create biblioteca.py

---
+++ modulos/biblioteca.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render_biblioteca_guias():
+    st.title("📚 Biblioteca de Guías y Materiales")
+    st.info("Descarga de guías, PDFs y recursos académicos.")
+    # Aquí irá el código de la biblioteca
