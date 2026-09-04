ba90a32ac857b9dba16d1d5105e2db26a8989ca6
alematradingacademymx-cloud <alematradingacademymx@gmail.com>
2026-09-04 02:03:26 -0600

    Create evaluaciones.py

---
+++ modulos/evaluaciones.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render_evaluaciones_control():
+    st.title("🎓 Evaluaciones y Control Académico")
+    st.info("Panel de exámenes, calificaciones y certificados.")
+    # Aquí irá el código de evaluaciones
