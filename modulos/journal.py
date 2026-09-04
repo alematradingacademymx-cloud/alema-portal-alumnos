86ae1e41658fa3bb6db4129f0efe147c3481526e
alematradingacademymx-cloud <alematradingacademymx@gmail.com>
2026-09-04 02:02:21 -0600

    Create journal.py

---
+++ modulos/journal.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render_trading_journal():
+    st.title("📓 Trading Journal")
+    st.info("Registro y bitácora de operaciones.")
+    # Aquí irá el código de tu journal
